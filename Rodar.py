e60bb8fcfddf2e453f2eb56171a197cd6365d9b4
Edresson Casanova <edresson1@gmail.com>
2017-12-11 21:03:04 -0200

    Add files via upload

---
+++ Rodar.py
@@ -0,0 +1,121 @@
+import os
+#Base
+base1_x = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Treino/Base1-X.txt '
+base1_y = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Treino/Base1-Y.txt '
+base2_pessoas = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base2-Pessoas.txt '
+base2_cadastrados = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base2-Cadastrados.txt '
+
+base3_pessoas = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base3-Pessoas.txt '
+base3_cadastrados = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base3-Cadastrados.txt '
+
+base1_cadastrados = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base1-Cadastrados.txt '
+base1_pessoas = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base1-Pessoas.txt '
+
+#Base18k todos
+
+
+base1_x_18k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Treino/Base1-X-18k.txt '
+base1_y_18k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Treino/Base1-Y-18k.txt '
+base2_pessoas_18k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base2-Pessoas-18k.txt '
+base2_cadastrados_18k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base2-Cadastrados-18k.txt '
+
+base3_pessoas_18k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base3-Pessoas-18k.txt '
+base3_cadastrados_18k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base3-Cadastrados-18k.txt '
+
+base1_cadastrados_18k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base1-Cadastrados-18k.txt '
+base1_pessoas_18k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base1-Pessoas-18k.txt '
+
+
+#Base48k
+
+
+base1_x_48k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Treino/Base1-X-48k.txt '
+base1_y_48k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Treino/Base1-Y-48k.txt '
+base2_pessoas_48k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base2-Pessoas-48k.txt '
+base2_cadastrados_48k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base2-Cadastrados-48k.txt '
+
+base3_pessoas_48k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base3-Pessoas-48k.txt '
+base3_cadastrados_48k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base3-Cadastrados-48k.txt '
+
+base1_cadastrados_48k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base1-Cadastrados-48k.txt '
+base1_pessoas_48k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base1-Pessoas-48k.txt '
+
+#Base44k
+base1_x_44k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Treino/Base1-X-44k.txt '
+base1_y_44k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Treino/Base1-Y-44k.txt '
+base2_pessoas_44k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base2-Pessoas-44k.txt '
+base2_cadastrados_44k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base2-Cadastrados-44k.txt '
+
+base3_pessoas_44k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base3-Pessoas-44k.txt '
+base3_cadastrados_44k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base3-Cadastrados-44k.txt '
+
+base1_cadastrados_44k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base1-Cadastrados-44k.txt '
+base1_pessoas_44k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base1-Pessoas-44k.txt '
+
+#Base32k
+
+base1_x_32k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Treino/Base1-X-32k.txt '
+base1_y_32k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Treino/Base1-Y-32k.txt '
+base2_pessoas_32k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base2-Pessoas-32k.txt '
+base2_cadastrados_32k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base2-Cadastrados-32k.txt '
+
+base3_pessoas_32k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base3-Pessoas-32k.txt '
+base3_cadastrados_32k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base3-Cadastrados-32k.txt '
+
+base1_cadastrados_32k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base1-Cadastrados-32k.txt '
+base1_pessoas_32k = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Mfcc-Save/Validacao/Base1-Pessoas-32k.txt '
+
+
+
+
+rede2_1=' Scripts/Redes/Encoder1.py '
+modelo_nome ='encoder.tflearn'
+modelo_dir_nome = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Modelos/'+modelo_nome
+
+rede2_1_1=' Scripts/Redes/Encoder1-1.py '
+modelo_nome1_1 ='encoder1-1.tflearn'
+modelo_dir_nome1_1 = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Modelos/'+modelo_nome1_1
+
+rede2_1_2=' Scripts/Redes/Encoder1-2.py '
+modelo_nome1_2 ='encoder1-2.tflearn'
+modelo_dir_nome1_2 = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Modelos/'+modelo_nome1_2
+
+rede2_2_1=' Scripts/Redes/Encoder2-1.py '
+modelo_nome2_1 ='encoder2-1.tflearn'
+modelo_dir_nome2_1 = ' /home/edresson/Pti-embbending/Encoder-MFCC/Automatizado/Modelos/'+modelo_nome2_1
+
+
+rede_tradicional = ' Scripts/Redes/rna-tradicional.py '
+
+#rede 2.1 22k
+os.system('python'+rede2_1+base1_x+base1_y+base2_pessoas+base2_cadastrados+base1_pessoas+base1_cadastrados+base3_pessoas+base3_cadastrados+' 216 572 '+modelo_dir_nome+' > ' + modelo_nome+'.txt &')
+#rede 2.1.1
+os.system('python'+rede2_1_1+base1_x+base1_y+base2_pessoas+base2_cadastrados+base1_pessoas+base1_cadastrados+base3_pessoas+base3_cadastrados+' 216 572 '+modelo_dir_nome1_1+' > ' + modelo_nome1_1+'.txt &')
+#rede 2.1.2
+os.system('python'+rede2_1_2+base1_x+base1_y+base2_pessoas+base2_cadastrados+base1_pessoas+base1_cadastrados+base3_pessoas+base3_cadastrados+' 216 572 '+modelo_dir_nome1_2+' > ' + modelo_nome1_2+'.txt &')
+
+
+#rede normalizada
+#os.system('python '+rede2_2_1+base1_x+base1_y+base2_pessoas+base2_cadastrados+base1_pessoas+base1_cadastrados+base3_pessoas+base3_cadastrados+' 216 572 '+modelo_dir_nome2_1+' > ' + modelo_nome2_1+'.txt &')
+
+#rede 2.1 16k
+os.system('python'+rede2_1+base1_x_18k+base1_y_18k+base2_pessoas_18k+base2_cadastrados_18k+base1_pessoas_18k+base1_cadastrados_18k+base3_pessoas_18k+base3_cadastrados_18k+' 157  416 '+modelo_dir_nome+' > ' + modelo_nome+'-16k'+'.txt &')
+
+
+#rede 2.1 48k
+os.system('python'+rede2_1+base1_x_48k+base1_y_48k+base2_pessoas_48k+base2_cadastrados_48k+base1_pessoas_48k+base1_cadastrados_48k+base3_pessoas_48k+base3_cadastrados_48k+' 469  1222 '+modelo_dir_nome+' > ' + modelo_nome+'-48k'+'.txt &')
+
+
+
+#rede 2.1 44k
+os.system('python'+rede2_1+base1_x_44k+base1_y_44k+base2_pessoas_44k+base2_cadastrados_44k+base1_pessoas_44k+base1_cadastrados_44k+base3_pessoas_44k+base3_cadastrados_44k+' 431  1131 '+modelo_dir_nome+' > ' + modelo_nome+'-44k'+'.txt &')
+
+#rede 2.1 32k
+os.system('python'+rede2_1+base1_x_32k+base1_y_32k+base2_pessoas_32k+base2_cadastrados_32k+base1_pessoas_32k+base1_cadastrados_32k+base3_pessoas_32k+base3_cadastrados_32k+' 313  819 '+modelo_dir_nome+' > ' + modelo_nome+'-32k'+'.txt &')
+
+
+
+
+#rede tradicional 
+os.system('python '+rede_tradicional+'  > Rede-Tradicional.txt &')
+
